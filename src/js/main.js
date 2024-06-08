84cbdde6fe009c88da9eba5833fe7603c178dd8e
TalehHesenov <talehhesenov19944@gmail.com>
2024-06-09 01:35:54 +0400

    new edit and new add

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,21 @@
 let dark = document.getElementById("dark");
+document.addEventListener("DOMContentLoaded", function() {
+  var dark = document.getElementById("dark");
+  var MainImg = document.getElementById("MainImg");
+  var smallimg = document.querySelectorAll(".small-img");
+
+  // Küçük resimlerin tıklama olaylarına dinleyiciler ekleyin
+  smallimg.forEach(function(img, index) {
+      img.onclick = function() {
+          MainImg.src = img.src;
+      }
+  });
+});
 
 
 
-  dark.addEventListener("click", function () {
+
+
+dark.addEventListener("click", function () {
     document.body.classList.toggle("dark_theme");
   });
