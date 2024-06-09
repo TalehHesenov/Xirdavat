3d45927bf326e6d1acf2f9f077a1ec64921adea9
TalehHesenov <talehhesenov19944@gmail.com>
2024-06-09 22:50:23 +0400

    responsiv add

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,39 @@ document.addEventListener("DOMContentLoaded", function() {
 dark.addEventListener("click", function () {
     document.body.classList.toggle("dark_theme");
   });
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // Menü simgesini seçin
+    const barsButton = document.querySelector('.bars_button');
+    const close_button = document.querySelector('.close_button');
+    // Menü listesini seçin
+    const menu = document.querySelector('nav ul');
+
+
+
+    // Menü simgesine tıklama olayı ekleyin
+    barsButton.addEventListener('click', function () {
+        // Menü açık mı kapalı mı kontrol edin
+        if (menu.style.right === '0px') {
+            // Menü açık ise kapat
+            menu.style.right = '-300px';
+        } else {
+            // Menü kapalı ise aç
+            menu.style.right = '0px';
+        }
+    });
+
+
+    close_button.addEventListener('click', function () {
+      // Menü açık mı kapalı mı kontrol edin
+      if (menu.style.right === '0px') {
+          // Menü açık ise kapat
+          menu.style.right = '-300px';
+      } else {
+          // Menü kapalı ise aç
+          menu.style.right = '0px';
+      }
+  });
+});
